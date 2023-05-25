4d937345079c98713789cac1495350a8209a86eb
Zisan <zikaplan42@gmail.com>
2023-05-25 17:01:14 +0300

    Initial commit

---
+++ src/main/java/com/tpe/repository/FilmRepository.java
@@ -0,0 +1,16 @@
+package com.tpe.repository;
+
+import com.tpe.domain.Film;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FilmRepository extends JpaRepository<Film, Integer> {
+
+    @Procedure(procedureName = "getCount")
+    String getFilmCount();
+
+
+
+}
